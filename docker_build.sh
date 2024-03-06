640d9fa96a3f83f28484c7bfafb1418c95d057a4
andrew-pineiro <apineiro@towerpower.com>
2024-03-06 15:47:40 -0500

    more

--- docker_build.sh
+++ docker_build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 rm /www/* -r
-cp /root/goweb/* . -r
+cp /root/goweb/* /www/ -r
+cd /www
 docker build --tag goweb .
 docker rm goweb_1
 docker run --name goweb_1 -p 80:8080 goweb:latest
