6670d228c9f7905fb1f2285c1ac39f9db9a2d983
andrew-pineiro <apineiro@towerpower.com>
2024-03-06 22:23:55 -0500

    build script fixes

--- build.sh
+++ build.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 set -xe
 
-APPDIR = "/www"
-DOCKNAME = "goweb_1"
-IMGNAME = "goweb"
+APPDIR="/www"
+DOCKNAME="goweb_1"
+IMGNAME="goweb"
 
-if [ -d $APPDIR]; then
+if [ -d $APPDIR ]; then
     rm $APPDIR -r
 fi
 mkdir $APPDIR
-cp "./*" $APPDIR -r
+cp ./ $APPDIR -r
 
 docker build --tag $IMGNAME $APPDIR
 
-if [ ! `$(docker ps -a -q -f name=$DOCKNAME)` ]; then
-    if [ `$(docker ps -aq -f status=exited -f name=$DOCKNAME)` ]; then
-        docker rm $DOCKNAME
-    fi
-    docker run --name $DOCKNAME -p 80:8080 $IMGNAME:latest
+if [ $( docker ps -a -f name=$DOCKNAME| wc -l ) -eq 2 ]; then
+  docker rm -f $DOCKNAME
 fi
+
+ docker run -d --name $DOCKNAME -p 80:8080 $IMGNAME:latest
