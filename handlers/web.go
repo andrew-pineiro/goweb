fb2c12f66772c9b05acccc1cfbf8f0f892e090a3
Andrew Pineiro <apineiro@towerpower.com>
2024-03-12 15:16:06 -0400

    allow use of non-html files due to base templating

--- handlers/web.go
+++ handlers/web.go
@@ -68,19 +68,36 @@ func LoadPage(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "404 page not found", http.StatusNotFound)
 		return
 	}
+
 	log.Printf("%s LOAD: %s", r.RemoteAddr, file)
 
-	tmpl, err := template.ParseFiles(file, baseFile)
-	if err != nil {
-		log.Printf("%s ERROR: %s", r.RemoteAddr, err.Error())
-		http.Error(w, "500 internal server error", http.StatusInternalServerError)
-		return
-	}
-	//TODO(#4): implement data injection to pages
-	data := ""
-	if err := tmpl.ExecuteTemplate(w, "base", data); err != nil {
-		log.Printf("%s ERROR: %s", r.RemoteAddr, err.Error())
-		http.Error(w, "500 internal server error", http.StatusInternalServerError)
-		return
+	if strings.Contains(file, ".html") {
+		tmpl, err := template.ParseFiles(file, baseFile)
+
+		if err != nil {
+			log.Printf("%s ERROR: %s", r.RemoteAddr, err.Error())
+			http.Error(w, "500 internal server error", http.StatusInternalServerError)
+			return
+		}
+
+		if err := tmpl.ExecuteTemplate(w, "base", data); err != nil {
+			log.Printf("%s ERROR: %s", r.RemoteAddr, err.Error())
+			http.Error(w, "500 internal server error", http.StatusInternalServerError)
+			return
+		}
+	} else {
+		tmpl, err := template.ParseFiles(file)
+
+		if err != nil {
+			log.Printf("%s ERROR: %s", r.RemoteAddr, err.Error())
+			http.Error(w, "500 internal server error", http.StatusInternalServerError)
+			return
+		}
+
+		if err := tmpl.Execute(w, data); err != nil {
+			log.Printf("%s ERROR: %s", r.RemoteAddr, err.Error())
+			http.Error(w, "500 internal server error", http.StatusInternalServerError)
+			return
+		}
 	}
 }
