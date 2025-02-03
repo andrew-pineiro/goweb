2ff2f96ff58896ad5d8b80962e6eb097f32c63f5
andrew-pineiro <chillsav12345@gmail.com>
2025-02-03 18:46:41 -0500

    docker fixes

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -2,21 +2,13 @@
 set -e  # Exit on error
 
 IMAGE_NAME="andrew-pineiro/goweb:latest"
-CONTAINER_NAME="goweb-builder"
 
-echo "Building Go application inside Docker..."
+cp Dockerfile ../src
 
-# Step 1: Use a temporary container to build the Go application
-docker run --rm \
-    -v "$(pwd)":/app \
-    -w /app \
-    golang:1.19-alpine \
-    sh -c "apk add --no-cache git && go mod download && go build -ldflags='-w -s' -o goweb"
-
-echo "Go application built successfully!"
-
-# Step 2: Build the final Docker image
 echo "Building Docker image..."
+pushd ../src
 docker build -t $IMAGE_NAME .
 
 echo "Docker build complete!"
+rm Dockerfile
+popd
